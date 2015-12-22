dd29980eda2516c127b9d49078039f78b3e588ec
Alice Xue <alicex123@gmail.com>
2015-12-21 23:43:09 -0500

    started chrome extension stuff

---
+++ popup.js
@@ -0,0 +1,20 @@
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    message.innerText = request.source;
+  }
+});
+
+function onWindowLoad() {
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getArticleHTML.js"
+  }, function() {
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
+}
+
+window.onload = onWindowLoad;
