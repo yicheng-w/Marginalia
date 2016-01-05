ff6c697ef226bc7ac93345a94104c46d410fe474
Alice Xue <alicex123@gmail.com>
2016-01-05 16:14:38 -0500

    minor bug fix

--- content.js
+++ content.js
@@ -2,6 +2,9 @@ console.log("loaded content.js");
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+	console.log(request);
+	console.log(sender);
+	console.log(sendResponse);
         if(request.method == "getHTML"){
 	    console.log(document.all[0].innerText);
             sendResponse({data: document.all[0].innerText,  method: "getHTML"});
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 console.log("loaded popup.js");
 
-chrome.tabs.query({currentWindow: true, active: true}, function(tabArray) {
+chrome.tabs.query({active: true}, function(tabArray) {
     currentTabID = tabArray[0].id;
     chrome.tabs.sendMessage(currentTabID, {method: "getHTML"}, function(response) {
         if(response.method=="getHTML"){
