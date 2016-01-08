c278718c092e7c0d2c96afc30a232d27998d5ef5
JeffreyZou13 <jeffreyzou13@gmail.com>
2016-01-07 19:16:41 -0500

    made some progress =)

--- content.js
+++ content.js
@@ -6,8 +6,16 @@ chrome.runtime.onMessage.addListener(
 	console.log(sender);
 	console.log(sendResponse);
         if(request.method == "getHTML"){
-	    console.log(document.body.outerHTML);
-            sendResponse({data: document.body.outerHTML,  method: "getHTML"});
+          console.log(document.getElementsByTagName("p"));
+          var paragraphs = document.getElementsByTagName("p");
+          var onlyP = [];
+          for (i=0;i<paragraphs.length;i++) {
+            console.log(paragraphs[i].classList.length == 0);
+            }
+          //console.log(onlyP);
+          sendResponse({data: document.getElementsByTagName("p"),  method: "getHTML"});
+	    //console.log(document.body.outerHTML);
+            //sendResponse({data: document.body.outerHTML,  method: "getHTML"});
         }
     }
 );
