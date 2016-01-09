495c0dd6b081cd384853deaedddee01b29f30d45
Alice Xue <alicex123@gmail.com>
2016-01-09 16:08:05 -0500

    gets text from paragraphs

--- content.js
+++ content.js
@@ -18,19 +18,32 @@ chrome.runtime.onMessage.addListener(
 	console.log(sender);
 	console.log(sendResponse);
         if(request.method == "getHTML"){
-          //console.log(document.getElementsByTagName("p"));
-          var paragraphs = document.getElementsByTagName("p");
-          var title = document.title;
-          var onlyP = [];
-          var pText = [];
-          for (i=0;i<paragraphs.length;i++) {
-            if (paragraphs[i].classList.length == 0) {
-                onlyP.push(paragraphs[i]);
-                //Take the text and put it into array
-                //NOTE: Firefox does NOT support innerText!
-                pText.push(paragraphs[i].innerText);
+            console.log(document.getElementsByTagName("p"));
+	    var divs = document.getElementsByTagName("div");
+            var paragraphs = document.getElementsByTagName("p");
+            var title = document.title;
+            var onlyP = [];
+            var pText = [];
+            for (i=0;i<paragraphs.length;i++) {
+		if (paragraphs[i].getAttribute("itemprop") == "articleBody") {
+		    onlyP.push(paragraphs[i]);
+		    //Take the text and put it into array
+		    //NOTE: Firefox does NOT support innerText!
+		    pText.push(paragraphs[i].innerText);
+		}
             }
-          }
+	    for (i=0;i<divs.length;i++) {
+		if (divs[i].getAttribute("itemprop") == "articleBody") {
+		    divParagraphs = divs[i].children;
+		    console.log(divParagraphs);
+		    for (j=0;j<divParagraphs.length;j++) {
+			if (divParagraphs[j].tagName == "P") {
+			    onlyP.push(divParagraphs[j]);
+			    pText.push(divParagraphs[j].innerText);
+			}
+		    }
+		}
+	    }
           //console.log("--------------------------");
           console.log("---------title------------");
           console.log(title);
