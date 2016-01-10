c2feea076ae2f4c9a46e2bd9c67f8c37126fb5a1
Alice Xue <alicex123@gmail.com>
2016-01-09 21:38:48 -0500

    gets dates and authors

--- content.js
+++ content.js
@@ -18,10 +18,56 @@ chrome.runtime.onMessage.addListener(
 	console.log(sender);
 	console.log(sendResponse);
         if(request.method == "getHTML"){
-            console.log(document.getElementsByTagName("p"));
+	    var metadata = document.getElementsByTagName("meta");
 	    var divs = document.getElementsByTagName("div");
+	    var spans = document.getElementsByTagName("span");
             var paragraphs = document.getElementsByTagName("p");
             var title = document.title;
+	    var dates = document.getElementsByTagName("time");
+	    var datePublished = "";
+	    if (typeof dates[0] != "undefined") {
+		datePublished = dates[0].innerText;
+	    }
+	    for (i=0;i<dates.length;i++) {
+		if (dates[i].getAttribute("itemprop") == "datePublished") {
+		    datePublished = dates[i].innerText;
+		}
+	    }
+	    if (datePublished == "") {
+		for (i=0;i<metadata.length;i++) {
+		    if (metadata[i].getAttribute("property") == "article:published_time") {
+			datePublished = metadata[i].getAttribute("content");
+			if (datePublished.length > 10) {
+			    datePublished = datePublished.substring(0,10);
+			}
+		    }
+		}
+	    }
+	    console.log(datePublished);
+	    var author = "";
+	    for (i=0;i<metadata.length;i++) {
+		if (metadata[i].getAttribute("name") == "author") {
+		    author = metadata[i].getAttribute("content");
+		    break;
+		}
+	    }
+	    if (author == "") {
+		for (i=0;i<spans.length;i++) {
+		    if (spans[i].class == "byline-author") {
+			author = spans[i].innerText;
+			break;
+		    }
+		}
+	    }
+	    if (author == "") {
+		var anchors = document.getElementsByTagName("a");
+		for (i=0;i<anchors.length;i++) {
+		    if (anchors[i].getAttribute("itemprop") == "author") {
+			author = anchors[i].innerText;
+		    }
+		}
+	    }
+	    console.log(author);
             var onlyP = [];
             var pText = [];
             for (i=0;i<paragraphs.length;i++) {
@@ -29,32 +75,48 @@ chrome.runtime.onMessage.addListener(
 		    onlyP.push(paragraphs[i]);
 		    //Take the text and put it into array
 		    //NOTE: Firefox does NOT support innerText!
-		    pText.push(paragraphs[i].innerText);
+		    pText.push(paragraphs[i].innerHTML);
 		}
             }
-	    for (i=0;i<divs.length;i++) {
-		if (divs[i].getAttribute("itemprop") == "articleBody") {
-		    divParagraphs = divs[i].children;
-		    console.log(divParagraphs);
-		    for (j=0;j<divParagraphs.length;j++) {
-			if (divParagraphs[j].tagName == "P") {
-			    onlyP.push(divParagraphs[j]);
-			    pText.push(divParagraphs[j].innerText);
+	    if (pText.length == 0) {
+		for (i=0;i<divs.length;i++) {
+		    if (divs[i].getAttribute("itemprop") == "articleBody" || divs[i].getAttribute("id") == "article-body") {
+			divParagraphs = divs[i].children;
+			console.log(divParagraphs);
+			for (j=0;j<divParagraphs.length;j++) {
+			    if (divParagraphs[j].tagName == "P") {
+				console.log(divParagraphs[j]);
+				console.log(divParagraphs[j].tagName);
+				onlyP.push(divParagraphs[j]);
+				pText.push(divParagraphs[j].innerHTML);
+			    }
 			}
 		    }
 		}
 	    }
-          //console.log("--------------------------");
-          console.log("---------title------------");
-          console.log(title);
-          console.log("-------Paragraphs---------");
-          console.log(pText);
-          var data = {
-            title:title,
-            p:paragraphs,
-            method:"getHTML"
+	    if (pText.length == 0) {
+		for (i=0;i<spans.length;i++) {
+		    if (spans[i].class == "s1") {
+			onlyP.push(spans[i]);
+			pText.push(spans[i].innerHTML);
+		    }
+		}
+	    }
+	    
+            //console.log("--------------------------");
+            console.log("---------title------------");
+            console.log(title);
+	    
+	    
+	    
+            console.log("-------Paragraphs---------");
+            console.log(pText);
+            var data = {
+		title:title,
+		p:paragraphs,
+		method:"getHTML"
           };
-          sendResponse(data);
+            sendResponse(data);
         }
     }
 );
