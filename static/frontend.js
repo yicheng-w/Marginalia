7e670d2e1400a5f5f583550684107f1b4647ab25
Ariel Levy <ariel.s.levy1@gmail.com>
2016-01-11 07:49:12 -0500

    updating stuff

--- static/frontend.js
+++ static/frontend.js
@@ -127,8 +127,11 @@ var formatComments = function formatComments() {
 var commentHoverOn = function commentHoverOn(num) {
 	var ctag = ".com-"+num;
 	$(".comment"+ctag).addClass("grey lighten-2");
-	$(".comment-block"+ctag).css("font-style","italic");
+//	$(".comment-block"+ctag).css("font-style","italic");
 	$(".comment-block"+ctag).addClass("z-depth-5");
+	$(".comment-block"+ctag).animate({
+		opacity: "1"
+	}, "fast")
 };
 
 /** Undos hover formatting for comment and comment block.
@@ -136,8 +139,11 @@ var commentHoverOn = function commentHoverOn(num) {
 var commentHoverOff = function commentHoverOff(num) {
 	var ctag = ".com-"+num;
 	$(".comment"+ctag).removeClass("grey lighten-2");
-	$(".comment-block"+ctag).css("font-style","normal");
+//	$(".comment-block"+ctag).css("font-style","normal");
 	$(".comment-block"+ctag).removeClass("z-depth-5");
+	$(".comment-block"+ctag).animate({
+		opacity: "0.85"
+	}, "fast")
 };
 
 /** Adds hover command to comments and comment blocks.
@@ -196,3 +202,36 @@ var runMarginalia = function runMarginalia() {
 	getSelectedText();
 };
 runMarginalia();
+
+function getOffset( el ) {
+    var rect = el.getBoundingClientRect();
+    return {
+        left: rect.left + window.pageXOffset,
+        top: rect.top + window.pageYOffset,
+        width: rect.width || el.offsetWidth,
+        height: rect.height || el.offsetHeight
+    };
+}
+
+function connect(div1, div2, thickness) { // draw a line connecting elements
+    var off1 = getOffset(div1);
+    var off2 = getOffset(div2);
+    // bottom right
+    var x1 = off1.left + off1.width;
+    var y1 = off1.top + off1.height;
+    // top right
+    var x2 = off2.left + off2.width;
+    var y2 = off2.top;
+    // distance
+    var length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+    // center
+    var cx = ((x1 + x2) / 2) - (length / 2);
+    var cy = ((y1 + y2) / 2) - (thickness / 2);
+    // angle
+    var angle = Math.atan2((y1-y2),(x1-x2))*(180/Math.PI);
+    // make hr
+    var htmlLine = "<div style='padding:0px; margin:0px; height:" + thickness + "px; background-color:#000" + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
+    //
+    // alert(htmlLine);
+    document.body.innerHTML += htmlLine;
+}
