f98e2685729d4a8fc2530ef8bb2ee0c2f86fc801
JeffreyZou13 <jeffreyzou13@gmail.com>
2016-01-18 19:14:46 -0500

    stuff happened

--- parseHTML.py
+++ parseHTML.py
@@ -1,5 +1,6 @@
 #################################################################################
 # Parse HTML for the project, handles the BeautifulSoup and HTML that is passed #
+# and connects it to the database                                               #
 #                                                                               #
 # Authors                                                                       #
 #  Jeffrey Zou                                                                  #
@@ -9,17 +10,40 @@
 #                                                                               #
 #################################################################################
 
+import sqlite3
 from bs4 import BeautifulSoup
-#import urllib2
+from database import *
 
-def pretty():
-    thePage = '''
-    <html>
-    <body>
-    <p>Hi</p>
-    </body>
-    </html>
+def pretty(htmlPage):
+    '''
+    Returns a pretty version of the HTML
+
+    Args:
+        htmlPage (string): the HTML that will be prettified
+
+    Returns:
+        Unicode of prettified HTML
     '''
 
-    soup = BeautifulSoup(thePage, "html.parser")
-    print soup.get_text()
+    soup = BeautifulSoup(htmlPage, "html.parser")
+    return soup.prettify()
+
+def updateDatabase(email, htmlPage, site_id, comments, notes):
+    '''
+    Updates the database with the page or adds it if it does not exist
+
+    Args:
+        htmlPage (string): the HTML of the page
+        email (string): user's email
+        site_id (integer): id of the site, -1 if it does not exist
+
+    Returns:
+        True
+    '''
+
+    page = pretty(htmlPage)
+    if site_id == -1:
+        add_to_sites(email, page, comments, notes)
+    else:
+        update_site(email, site_id, page, comments, notes)
+    return True
